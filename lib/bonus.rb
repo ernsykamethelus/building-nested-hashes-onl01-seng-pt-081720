84978ebcc347d6ba4baaa87d1d2a2ade792daa0c
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-30 03:25:23 +0000

    Automatically backed up by Learn

---
+++ lib/bonus.rb
@@ -0,0 +1,5 @@
+def bonus 
+	xit "uses the []= method to change the hero and heroine's status from alive to dead" do 
+    bonus[:montague][:hero][:status])=("dead")
+    (bonus[:capulet][:heroine][:status])=("dead")
+end
